e68284bbee4ea4caa1cbb5f38e4fab3b03781596
Rybin Artem <rybin.ar@citilink.ru>
2023-10-02 16:56:49 +0300

    initial commit

--- cmd/auth/config.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Config struct {
-	HttpPublicListen string `long:"http-public-listen" description:"Listening host:port for public http-server" env:"HTTP_PUBLIC_LISTEN" required:"true"`
-	GrpcListen       string `long:"grpc-listen" description:"Listening host:port for grpc-server" env:"GRPC_LISTEN" required:"true"`
-}
